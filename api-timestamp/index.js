fdef06038bb302953a06d2a392d93bb65e613670
Home <nnee@edsfds.rr>
2024-09-03 05:05:54 +0000

    Public Api

---
+++ api-timestamp/index.js
@@ -0,0 +1,67 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// Обработка API запроса на получение времени
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) 
+  {
+    date = new Date();
+  }
+  else 
+  {
+    // Проверяем, является ли входной параметр числом (Unix timestamp)
+    if (!isNaN(dateString)) 
+    {
+      date = new Date(parseInt(dateString));
+    } 
+    else 
+    {
+      date = new Date(dateString);
+    }
+  }
+
+  // Проверка даты на валидность
+  if (date.toString() === "Invalid Date") 
+  {
+    res.json({ error: "Invalid Date" });
+  } 
+  else 
+  {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+  
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
